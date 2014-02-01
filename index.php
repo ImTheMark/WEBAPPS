873c57dc172f7452c32419ea3a46674971173335
givmefive5 <givme_five5@yahoo.com>
2014-02-01 13:17:54 +0800

    gamitin niyo

--- index.php
+++ index.php
@@ -5,13 +5,20 @@
 		<link href="css/customstyles.css" rel="stylesheet">
 	</head>
 	<body>
-		<?php include('includes/nav.php'); ?>
-		<?php include('model/eventmodel.php'); ?>
+		<?php include('includes/nav.php');
+			include('model/eventmodel.php');?>
 		
+		<button type="submit" onclick = "myfunc()">Submit</button>
+		<p id ="S"> HELLO </p>
 		
-		
-		
-		
+		<script>
+			function myfunc(){
+				x = document.getElementById('S');
+				x.innerHTML = "<?php
+						echo "HESS";
+					?>";
+			}
+		</script>
 		<script src="js/jquery-1.10.2.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 	</body>
