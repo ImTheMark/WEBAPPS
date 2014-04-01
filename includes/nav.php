3d56db71157d51f1056d101fafe0996f96882e97
ImTheMark <mark_serapion@yahoo.com>
2014-04-01 21:49:14 +0800

    Fix

--- includes/nav.php
+++ includes/nav.php
@@ -1,35 +1,27 @@
 
 <div class="container">
-        <a href="index.php"><img src="images/logo-2.png" class="img-responsive"/></a>
-		<div class="navbar navbar-default navbar-inverse" role="navigation">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-				<li><a href="search.php">Browse Events</a></li>
-				<li><a href="company.php">Companies</a></li>
-				<li><a href="contact.php">Contact Us</a></li>
-				
-          </ul>
-          <ul class="nav navbar-nav pull-right">
-			<li><a href="apply.php" style="color:rgb(208, 133, 214);font-size:15px" onMouseOver="this.style.color='rgb(255, 255, 255)'"
-   onMouseOut="this.style.color='rgb(208, 133, 214)'"><b> WANT TO ADVERTISE YOUR EVENTS? APPLY NOW!</b></a></li>
-				
-          </ul>
-      
-		<!--
-		<form action='search.php' class="navbar-form navbar-right" role="search" method="GET">
-	        <div class="form-group">
-	          <input name='s' type="text" class="form-control" placeholder="Search Events...">
-	        </div>
-	        <button type="submit" class="btn btn-primary">Search</button>
-	    </form>-->
-        </div><!--/.nav-collapse -->
- </div>
+  <a href="index.php"><img src="images/logo-2.png" class="img-responsive"/></a>
+  <div class="navbar navbar-default navbar-inverse" role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="search.php">Browse Events</a></li>
+        <li><a href="company.php">Companies</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+
+      </ul>
+      <ul class="nav navbar-nav pull-right">
+       <li><a href="apply.php" style="color:rgb(208, 133, 214);font-size:15px" onMouseOver="this.style.color='rgb(255, 255, 255)'"
+         onMouseOut="this.style.color='rgb(208, 133, 214)'"><b> WANT TO ADVERTISE YOUR EVENTS? APPLY NOW!</b></a></li>
+
+       </ul>
+     </div>
+   </div>
 
